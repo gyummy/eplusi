417d0a519e82366a7bd994940ead1add6e6100c4
initialkommit <initialkommit@gmail.com>
2018-04-23 22:08:17 +0900

    settings 분리

---
+++ eplusi/settings/production.py
@@ -0,0 +1,12 @@
+from .base import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['']
+
+DATABASES = {
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'name',
+    'USER': 'user',
+    'PASSWORD': 'password'
+}
